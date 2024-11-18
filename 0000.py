d37168320ce58cd4edff9a8b4939674af4bf7309
Lucas Pires <lucasopf95@gmail.com>
2024-11-18 17:46:41 -0300

    Documentation and metadata

--- 0000.py
+++ 0000.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python
+"""Lang Identify.
 
-print("Hello, friend.")
+Indentifying  a system language configuration.
+
+Usage: 
+
+Have the LANG variable properly configured:
+
+    export LANG=pt_BR
+
+Execution:
+
+    python 0000.py
+    or
+    ./0000.py
+    or
+    LANG=pt_BR.utf8 python 0000.py
+"""
+__version__ = "0.0.1"
+__author__ = "Lcspires"
+__licence__ = "Unlicense"
+
+import os
+
+current_language  = os.getenv("LANG", "en_US")[:5]
+
+if current_language == ("en_US"):
+    print("Hello, friend.")
+elif current_language == ("pt_BR"):
+    print("Olá, garoto!")
+    
+#Respect the PEP 8.
